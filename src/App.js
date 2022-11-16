725cce14c512bc5f3d804135c3faf3885a1af009
unknown <vasudevanaidukilaparthi045@gmail.com>
2022-11-16 11:01:32 +0530

    broken our webpage into smaller components , finished stylling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Homepage from './homepage.component';
+import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
@@ -11,4 +11,4 @@ function App() {
   );
 }
  
-export default App;
+export default App; 
